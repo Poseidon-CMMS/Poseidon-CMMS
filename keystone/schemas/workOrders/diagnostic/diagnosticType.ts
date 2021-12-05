7ab8ef9f41d45df52c519727eb36845a29d25863
Mauricio Lima <limamauricio303@gmail.com>
2021-12-05 01:23:30 -0300

    Adds diagnostic types to hdw issues

---
+++ keystone/schemas/workOrders/diagnostic/diagnosticType.ts
@@ -0,0 +1,37 @@
+import { list } from "@keystone-next/keystone";
+
+import { text, select, relationship } from "@keystone-next/keystone/fields";
+
+export const diagnosticType = list({
+  ui: {
+    isHidden: true,
+    listView: {
+      initialColumns: ['name', 'type'],
+    },
+  },
+  fields: {
+    name: text({ isRequired: true, isIndexed: 'unique' }),
+    type: select({
+        isRequired: true,
+        options: [
+          { label: 'Pressure Sensor', value: 'SPRES' },
+          { label: 'Gateway', value: 'GTW' },
+          { label: 'GPS Node', value: 'gpsNode' },
+          { label: 'LoRa', value: 'lora' },
+        ],
+        ui: {
+          displayMode: 'segmented-control',
+        },
+      }),
+      hdwIssue: relationship({
+        ref: 'HdwIssue.diagnosticType',
+        ui: {
+          displayMode: 'cards',
+          cardFields: ['irrigator', 'creationDate'],
+          linkToItem: true,
+          inlineConnect: true,
+        },
+        many: true,
+      }),
+  },
+});
